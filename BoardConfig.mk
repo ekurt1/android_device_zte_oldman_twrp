c6d94088579659cb4218ccc04179b331f0aebd2b
imasaru <evanmr4@gmail.com>
2017-09-09 17:43:50 -0500

    BoardConfig.mk: reorganization; added prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,19 +5,24 @@ USE_CAMERA_STUB := true
 
 #Architecture
 TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := msm8909
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+
 
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := oldman
+# TARGET_BOOTLOADER_BOARD_NAME := oldman
+
+# Board
+TARGET_BOARD_PLATFORM := msm8909
+TARGET_NO_BOOTLOADER := true
 
 #Kernel
+# --We have kernel source, so no need for prebuilt--
+TARGET_PREBUILT_KERNEL := device/zte/oldman/kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
@@ -29,10 +34,16 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 4294950400
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# We have kernel source, so no need for prebuilt
-TARGET_PREBUILT_KERNEL := device/zte/oldman/kernel
-
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 # Recovery
-RECOVERY_VARIANT := twrp
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# TWRP
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+TW_THEME := portrait_hdpi
+TW_INCLUDE_CRYPTO := true
+TW_DEFAULT_BRIGHTNESS := 127
+TW_NO_REBOOT_BOOTLOADER := true
+RECOVERY_SDCARD_ON_DATA := true
